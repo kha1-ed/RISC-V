8ce172e16244cd95ed2df3454b5e6cecc17584c2
Omar Ashraf Abd El Mongy <128651431+OmarMongy@users.noreply.github.com>
2023-10-07 20:54:49 +0300

    Update Data_memory.v

--- Memory modules/Data_memory.v
+++ Memory modules/Data_memory.v
@@ -1,1 +1,29 @@
-
+module data_memory (
+    input clk, reset, 
+    input [31:0] WD,        // Write Data
+    input [31:0] A,         // Address
+    input WE,                // Write Enable
+    output reg [31:0] RD    // Read Data
+    );
+    
+    reg [31:0] memory [0:2**32-1];
+    integer i;
+    always@(posedge clk, negedge reset) begin
+        if(!reset) begin
+         for(i = 0; i < 2**32-1 ; i = i+ 1)begin
+         memory[i] <= 0;
+         
+         end
+         
+        end
+        
+        else if(WE)
+            memory[A] <= WD ;    // Write data at the specified address on rising edge of clock
+    end
+     
+    always@(*)
+      begin
+        RD = memory[A];       // Read data from the specified address
+      end   
+                       
+endmodule
