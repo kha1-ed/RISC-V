3728398d645f6bc885a683b2ce4f7fa3aeb1b3b9
Omar Ashraf Abd El Mongy <128651431+OmarMongy@users.noreply.github.com>
2023-10-07 22:24:30 +0300

    Update Data_memory.v

--- Memory modules/Data_memory.v
+++ Memory modules/Data_memory.v
@@ -1,29 +1,27 @@
-module data_memory (
-    input clk, reset, 
-    input [31:0] WD,        // Write Data
-    input [31:0] A,         // Address
-    input WE,                // Write Enable
-    output reg [31:0] RD    // Read Data
-    );
-    
-    reg [31:0] memory [0:2**32-1];
-    integer i;
-    always@(posedge clk, negedge reset) begin
-        if(!reset) begin
-         for(i = 0; i < 2**32-1 ; i = i+ 1)begin
-         memory[i] <= 0;
-         
-         end
-         
-        end
-        
-        else if(WE)
-            memory[A] <= WD ;    // Write data at the specified address on rising edge of clock
-    end
-     
-    always@(*)
-      begin
-        RD = memory[A];       // Read data from the specified address
-      end   
-                       
+module Data_memory
+#(parameter WIDTH = 32, DEPTH = 8)
+(
+input clk, reset_n, WE,
+input [DEPTH-1:0] addr,
+input [WIDTH-1:0] WD,
+output reg [WIDTH-1:0] RD,
+);
+reg [WIDTH-1:0] ram [0:2**DEPTH-1];
+integer i;
+always@(posedge clk, negedge reset_n)
+begin
+	if(!reset_n)
+		begin
+		     for(i = 0; i < 2**DEPTH-1 ; i = i + 1)
+		     begin
+			ram[i] <= 32'd0;
+		     end
+		end
+ 	else if(WE)
+  		ram[addr] <= WD;
+end 
+always@(*)
+begin
+RD = ram[addr];
+end
 endmodule
