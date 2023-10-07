37ff369e84cf1a642ed0c95f9c29b297d4eefd84
Omar Ashraf Abd El Mongy <128651431+OmarMongy@users.noreply.github.com>
2023-10-07 22:14:04 +0300

    Update Register file.v

--- Modules/Register file.v
+++ Modules/Register file.v
@@ -1,5 +1,5 @@
 module Register_file(
-      input clk, reset,
+      input clk, reset_n,
       input [4:0] A1, A2,            // Read ports 1 and 2 address
       input [4:0] A3,                // Write address
       input [31:0] WD3,              // Write data
@@ -7,22 +7,22 @@ module Register_file(
       output reg [31:0] RD1, RD2     // Read data 1 and 2
       );
 
-      reg [31:0] register [0:(2**5)-1];   // Array to hold registers
-      integer k ; 
+      reg [31:0] RF [0:31];   // Array to hold registers
+      integer i ; 
    
-      always @(posedge clk, negedge reset) begin
-        if(!reset) begin
-           for(k=0; k < 2**5 - 1 ; k = k + 1)
-           register[k] <= 0 ;   
+      always @(posedge clk, negedge reset_n) begin
+        if(!reset_n) begin
+              for(i=0; i < 2**5 - 1 ; i = i + 1)
+                    RF[i] <= 32'd0 ;   
         end
         
         else if (WE3) 
-          register[A3] <= WD3;            // Write operation on rising edge of clock
+          RF[A3] <= WD3;            // Write operation on rising edge of clock
       end
       
       always @(*) begin
-          RD1 <= register[A1];         // Read date
-          RD2 <= register[A2];
+          RD1 = RF[A1];         // Read date
+          RD2 = RF[A2];
       end
 
 endmodule
