0855ee00fef3e89ed607d28f715b3120e906eb5a
Omar Ashraf Abd El Mongy <128651431+OmarMongy@users.noreply.github.com>
2023-10-07 22:12:01 +0300

    Update PC.v

--- Modules/PC.v
+++ Modules/PC.v
@@ -1,16 +1,14 @@
 module PC_counter(
     input  clk,
     input  reset_n,
-    input  [31:0] PCNext,
-    output reg [31:0] PC
-    );
-        
-    always @(posedge clk, negedge reset_n) begin 
-          // PC <= 32'b0 ; 
-        if (~reset_n)
-            PC <= 'h0 ;
-        else
-            PC <= PCNext ;
-    end
-    
+    input  [31:0] inp,
+    output reg [31:0] out
+    );       
+always @(posedge clk, negedge reset_n)
+begin 
+ if (!reset_n)
+     out <= 32'd0;
+else
+     out <= inp;
+end
 endmodule
