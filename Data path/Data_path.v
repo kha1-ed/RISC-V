b10b66f05e9438b27f49333fe84e3dd389cc66b3
Omar Ashraf Abd El Mongy <128651431+OmarMongy@users.noreply.github.com>
2023-10-07 22:32:37 +0300

    Update Data_path.v

--- Data path/Data_path.v
+++ Data path/Data_path.v
@@ -1,1 +1,14 @@
+module Data_path(
+input clk, reset_n,
+  input [1:0] ResultSrc, ImmSrc,
+  input MemWrite, ALUSrc, RegWrite, PCSrc, 
+  input [2:0] ALUControl,
+  input [31:0] Instr, ReadData,
+  output [31:0] PC,
+  output [31:0] DataAdr,
+  output [31:0] WriteData,
+  output Zero
+);
 
+  
+endmodule
